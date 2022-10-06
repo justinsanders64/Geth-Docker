e2acf45313fa6c3be43a4153330d8e6ab2759440
joshua.kemp <joshua.kemp@hii-tsd.com>
2022-10-06 14:13:14 -0400

    Finalization of Docker Setup

--- build-dockers.sh
+++ build-dockers.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-docker build -f dockerfile -t geth-client . && \
-docker build -f dockerfile.node1 -t geth-client/node1 . && \
-docker build -f dockerfile.node2 -t geth-client/node2 .
+# docker build --build-arg ACCOUNT_PASSWORD=boot -f dockerfile.geth -t geth-client:boot . && \
+# docker build --build-arg ACCOUNT_PASSWORD=rpc -f dockerfile.geth -t geth-client:rpc . && \
+# docker build --build-arg ACCOUNT_PASSWORD=node1 -f dockerfile.geth -t geth-client:node1 . && \
+# docker build --build-arg ACCOUNT_PASSWORD=node2 -f dockerfile.geth -t geth-client:node2 . && \
+docker build -f dockerfile.ubuntu -t ubuntu-with-tools . && \
+docker build --build-arg ACCOUNT_PASSWORD=boot -f dockerfile.full -t geth-ubuntu:boot . && \
+docker build --build-arg ACCOUNT_PASSWORD=rpc -f dockerfile.full -t geth-ubuntu:rpc . && \
+docker build --build-arg ACCOUNT_PASSWORD=node1 -f dockerfile.full -t geth-ubuntu:node1 . && \
+docker build --build-arg ACCOUNT_PASSWORD=node2 -f dockerfile.full -t geth-ubuntu:node2 .
