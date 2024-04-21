9300d18c5f999c2ab1a0a9948ddbc48dd64fe44b
Justin Sanders <justinsanders@ksu.edu>
2024-04-21 01:00:22 -0500

    bug fix

--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ beacon-chain --datadir beacondata --min-sync-peers 0 --genesis-state genesis.ssz
 validator --datadir validatordata --accept-terms-of-use --interop-num-validators 64 --chain-config-file config.yml &
 geth --datadir gethdata --nodekeyhex 3028271501873c4ecf501a2d3945dcb64ea3f27d6f163af45eb23ced9e92d85b --networkid 1234565 --allow-insecure-unlock \
     --unlock 0x44B3Cbc9d59738A89B974c12623Cf5219C3b6b32 --password ~/password --http --http.addr 0.0.0.0 --http.port 8545 --http.api eth,net,web3 --http.corsdomain '*' \
-    --ws --ws.api eth,net,web3 --ws.origins '*' --netrestrict 10.1.0.0/22 --nat extip:10.1.0.10 --port 30303"
+    --ws --ws.api eth,net,web3 --ws.origins '*' --netrestrict 10.1.0.0/22 --nat extip:10.1.0.10 --port 30303
